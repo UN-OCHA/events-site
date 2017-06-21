9ca1ddc92979ed5c0ccefcd85c120b886fd9296f
Peter Droogmans <peter@attiks.com>
2017-06-21 23:02:41 +0200

    Chromedriver

--- behat/scripts/setup.sh
+++ behat/scripts/setup.sh
@@ -2,6 +2,7 @@ composer install
 ln -s ../html/sites sites
 wget -N https://chromedriver.storage.googleapis.com/2.30/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
+ls
 cd ../html/sites/all/themes/custom/ocha-basic
 npm install
 # grunt: Loading "svgmin.js" tasks...ERROR >> SyntaxError: Use of const in strict mode.
