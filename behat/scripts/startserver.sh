86ba66fe56895a486f8f54e824bc8db0e3d88b90
Peter Droogmans <peter@attiks.com>
2017-06-21 19:42:10 +0200

    Add debugging

--- behat/scripts/startserver.sh
+++ behat/scripts/startserver.sh
@@ -1,3 +1,3 @@
-phantomjs-2.1.1-linux-x86_64/bin/phantomjs --webdriver=4444  > /dev/null &
+phantomjs-2.1.1-linux-x86_64/bin/phantomjs --webdriver=4444 &
 cd ../html/sites/test/
-../../../behat/bin/drush runserver 8888 > /dev/null &
+../../../behat/bin/drush runserver 8888 &
