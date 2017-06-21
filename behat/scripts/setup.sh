d390a9ede123a19c55fda2e3b99335d44fadc26f
Peter Droogmans <peter@attiks.com>
2017-06-21 22:27:38 +0200

    Switch to chrome (driver)

--- behat/scripts/setup.sh
+++ behat/scripts/setup.sh
@@ -1,5 +1,4 @@
 composer install
 ln -s ../html/sites sites
-wget -N https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
-tar xvf phantomjs-2.1.1-linux-x86_64.tar.bz2
-chmod u+x phantomjs-2.1.1-linux-x86_64/bin/phantomjs
+wget -N https://chromedriver.storage.googleapis.com/2.30/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
