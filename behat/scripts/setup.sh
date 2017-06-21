6a3383ae80c5bf28ec6d9b45a697f6967d2e791d
Peter Droogmans <peter@attiks.com>
2017-06-21 17:24:42 +0200

    ES-67: Use local phantomjs

--- behat/scripts/setup.sh
+++ behat/scripts/setup.sh
@@ -1,2 +1,5 @@
 composer install
 ln -s ../html/sites sites
+wget -N https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+tar xvf phantomjs-2.1.1-linux-x86_64.tar.bz2
+chmod u+x phantomjs-2.1.1-linux-x86_64/bin/phantomjs
