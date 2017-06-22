a64ef96fc1f7e728765cf930e75c555d74cdee35
Peter Droogmans <peter@attiks.com>
2017-06-22 09:14:12 +0200

    Remove debugging

--- behat/scripts/setup.sh
+++ behat/scripts/setup.sh
@@ -2,7 +2,6 @@ composer install
 ln -s ../html/sites sites
 wget -N https://chromedriver.storage.googleapis.com/2.30/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
-ls
 cd ../html/sites/all/themes/custom/ocha-basic
 npm install
 grunt
