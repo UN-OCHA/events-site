d932fa9965ad206b0a401909d5c05d3d982cd9b2
Peter Droogmans <peter@attiks.com>
2017-10-05 10:41:09 +0200

    Trigger change

--- behat/scripts/install.sh
+++ behat/scripts/install.sh
@@ -1,5 +1,6 @@
 cp scripts/sites.php ../html/sites/
 cd ../html
+
 export PHP_OPTIONS="-d sendmail_path=`which true`"
 ../behat/bin/drush site-install standard --db-url=sqlite://sites/all/test.db --sites-subdir=test --account-pass=admin -y
 cd sites/test
