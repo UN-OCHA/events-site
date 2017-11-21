0173fa4391182ce12142fd559d7120157183ecaf
Peter Droogmans <peter@attiks.com>
2017-11-21 15:01:34 +0100

    OPS-2916: Add upgrade script, use exit codes

---
+++ update_modules/run_upgrade.sh
@@ -0,0 +1,37 @@
+# Setup.
+composer install
+npm i
+
+# Fetch a login link and create the cookie.
+docker exec -i events-local-php drush --root=/srv/www/html uli > login_link.txt
+php createCookie.php
+
+# Create baseline images.
+npm run reference
+
+# Run contrib updates, exclude core.
+docker exec -i events-local-php drush --root=/srv/www/html up --no-core -y > update.txt
+
+# Nothing updated, exit.
+[[ -z $(git status -uno --porcelain) ]] && echo "no updates needed" && exit 0
+
+# Create screenshots
+npm run reference-anon
+
+if [ ! $? -eq 0 ]; then
+  echo "Errors in anonymous screenshots"
+  exit 11
+fi
+
+npm run reference-auth
+
+if [ ! $? -eq 0 ]; then
+  echo "Errors in authenticated screenshots"
+  exit 12
+fi
+
+# No regressions, create branch
+branchname=`date +%Y-%m-%d`-update-contrib
+git checkout -b $branchname
+git commit -am 'Update contrib modules'
+git push origin $branchname
