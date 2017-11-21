5908487fd9646232c6fc41c322b907795cb81203
Peter Droogmans <peter@attiks.com>
2017-11-21 15:15:36 +0100

    OPS-2916: Upgrade script clean up

--- update_modules/run_upgrade.sh
+++ update_modules/run_upgrade.sh
@@ -13,7 +13,7 @@ npm run reference
 docker exec -i events-local-php drush --root=/srv/www/html up --no-core -y > update.txt
 
 # Nothing updated, exit.
-[[ -z $(git status -uno --porcelain) ]] && echo "no updates needed" && exit 0
+[[ -z $(git status -uno --porcelain ../html) ]] && echo "no updates needed" && exit 0
 
 # Create screenshots
 npm run reference-anon
@@ -30,8 +30,15 @@ if [ ! $? -eq 0 ]; then
   exit 12
 fi
 
-# No regressions, create branch
+## Clean up pre commit
+git checkout .
+rm package-lock.json
+
+# No regressions, create branch.
 branchname=`date +%Y-%m-%d`-update-contrib
 git checkout -b $branchname
-git commit -am 'Update contrib modules'
+
+# Add changed/added contrib files.
+git add ../html/sites/all/modules/contrib/
+git commit -m 'Update contrib modules'
 git push origin $branchname
