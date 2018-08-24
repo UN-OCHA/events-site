2b70af3cc556b29e14e2c3bca297e76611366d88
Chris Ruppel <chris@humanitarianresponse.info>
2018-08-24 14:04:54 +0200

    ES-150: new gulp workflow

--- behat/scripts/setup.sh
+++ behat/scripts/setup.sh
@@ -3,5 +3,6 @@ ln -s ../html/sites sites
 wget -N https://chromedriver.storage.googleapis.com/2.36/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 cd ../html/sites/all/themes/custom/ocha-basic
+export NODE_ENV='production'
 npm install
-grunt
+gulp build
