a5ceac64fc9c8a19ca4ebd740e483193e3a7e1d3
Chris Ruppel <chris@humanitarianresponse.info>
2018-08-24 14:30:50 +0200

    ES-150: allow devDeps to be installed but compile for prod

--- behat/scripts/setup.sh
+++ behat/scripts/setup.sh
@@ -3,7 +3,7 @@ ln -s ../html/sites sites
 wget -N https://chromedriver.storage.googleapis.com/2.36/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 cd ../html/sites/all/themes/custom/ocha-basic
-export NODE_ENV='production'
 npm install -g gulp-cli
 npm install
+export NODE_ENV='production'
 gulp build
